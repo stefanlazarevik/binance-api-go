e34ef172723fd643c4fe1d14b77fe4b7401a0e12
alexkozminski <102160228+alexkozminski@users.noreply.github.com>
2021-07-24 11:36:06 +0300

    * updated candlestick struct

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/posipaka-trade/binance-api-go
 
 go 1.16
 
-require github.com/posipaka-trade/posipaka-trade-cmn v0.0.0-20210724071620-838f8f64ddea
+require github.com/posipaka-trade/posipaka-trade-cmn v0.0.0-20210724082626-90eee1256676
--- internal/bncresponse/candle_stick_parser.go
+++ internal/bncresponse/candle_stick_parser.go
@@ -7,34 +7,35 @@ import (
 	"net/http"
 )
 
-func GetCandlestick(response *http.Response) (exchangeapi.Candlesticks, error) {
+func GetCandlestick(response *http.Response) (exchangeapi.Candlestick, error) {
 	_, err := getResponseBody(response)
 	if err != nil {
-		return exchangeapi.Candlesticks{}, err
+		return exchangeapi.Candlestick{}, err
 	}
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		return exchangeapi.Candlesticks{}, err
+		return exchangeapi.Candlestick{}, err
 	}
 	//var candleData Candlesticks
-	var c exchangeapi.Candlesticks
+	var c exchangeapi.Candlestick
 	var v []interface{}
 	if err := json.Unmarshal(body, &v); err != nil {
-		return exchangeapi.Candlesticks{}, err
+		return exchangeapi.Candlestick{}, err
 	}
 
-	c.OpenTime, _ = v[0].(int64)
-	c.Open, _ = v[1].(string)
-	c.High, _ = v[2].(string)
-	c.Low, _ = v[3].(string)
-	c.Close, _ = v[4].(string)
-	c.Volume, _ = v[5].(string)
-	c.CloseTime, _ = v[6].(int64)
-	c.QuoteAssetVolume, _ = v[7].(string)
-	c.NumberOfTrade, _ = v[8].(int64)
-	c.TakerBuyBaseAssetVolume, _ = v[9].(string)
-	c.TakerBuyQuoteAssetVolume, _ = v[10].(string)
-	c.Ignore, _ = v[11].(string)
+	// TODO rename field according to new struct
+	//c.OpenTime, _ = v[0].(int64)
+	//c.Open, _ = v[1].(string)
+	//c.High, _ = v[2].(string)
+	//c.Low, _ = v[3].(string)
+	//c.Close, _ = v[4].(string)
+	//c.Volume, _ = v[5].(string)
+	//c.CloseTime, _ = v[6].(int64)
+	//c.QuoteAssetVolume, _ = v[7].(string)
+	//c.NumberOfTrade, _ = v[8].(int64)
+	//c.TakerBuyBaseAssetVolume, _ = v[9].(string)
+	//c.TakerBuyQuoteAssetVolume, _ = v[10].(string)
+	//c.Ignore, _ = v[11].(string)
 
 	return c, nil
 }
