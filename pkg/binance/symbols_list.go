5d26a2fac712fa97d5c3dc310231295e95f506d7
alexkozminski <102160228+alexkozminski@users.noreply.github.com>
2021-09-13 17:53:56 +0300

    StoreSymbolLimits func was added

--- pkg/binance/symbols_list.go
+++ pkg/binance/symbols_list.go
@@ -4,34 +4,24 @@ import (
 	"github.com/posipaka-trade/posipaka-trade-cmn/exchangeapi/symbol"
 )
 
-//func (manager *ExchangeManager) UpdateSymbolsList() error {
-//	req, err := http.NewRequest(http.MethodGet, baseUrl+exchangeInfoEndpoint, nil)
-//	if err != nil {
-//		return err
-//	}
-//
-//	response, err := manager.client.Do(req)
-//	if err != nil {
-//		return err
-//	}
-//
-//	defer bncresponse.CloseBody(response)
-//	manager.symbolsList, err = mktdata.GetSymbolsList(response)
-//	if err != nil {
-//		return err
-//	}
-//	return nil
-//}
-
 func (manager *ExchangeManager) GetSymbolsList() ([]symbol.Assets, error) {
 	limitsArr, err := manager.GetSymbolLimits()
 	if err != nil {
 		return []symbol.Assets{}, err
 	}
+
 	var assetsArr []symbol.Assets
+
 	for i := 0; i < len(limitsArr); i++ {
 		asset := limitsArr[i].Assets
 		assetsArr = append(assetsArr, asset)
 	}
+
 	return assetsArr, nil
+}
+
+func (manager *ExchangeManager) StoreSymbolLimits(limits []symbol.Limits) {
+
+	manager.symbolsLimits = limits
+
 }
